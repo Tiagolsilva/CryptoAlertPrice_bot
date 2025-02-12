129618b6c4c5827d08525cad331e5fee1d6b717c
Tiago Silva <60697393+Tiagolsilva@users.noreply.github.com>
2025-02-11 21:09:33 -0300

    Create bot_telegram.py

---
+++ bot_telegram.py
@@ -0,0 +1,18 @@
+import requests
+
+# Configurações
+TELEGRAM_BOT_TOKEN = "8127477680:AAFzs5tPpThWgo9PrCbLzjPli5FCqZczmpQ"
+CHAT_ID = "7540701922"
+
+def send_telegram_message(message):
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    data = {"chat_id": CHAT_ID, "text": message}
+    response = requests.post(url, data=data)
+    
+    if response.status_code == 200:
+        print("✅ Message sent successfully.") 
+    else: 
+        print(f"❌ Failed to send message. Error: {response.text}")
+
+# Testando o envio da mensagem
+send_telegram_message("Hello, this is a test message from your bot!")
